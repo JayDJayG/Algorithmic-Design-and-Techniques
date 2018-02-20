d01a39c15dc510c0e1fb5c6b8f8a286243dce39f
Jay Guzman <j.d.guzman.v@gmail.com>
2018-02-20 16:26:15 -0700

    Stress testing following the course documentation

--- Challenge1_2MaximumPairwiseProduct/mpp.py
+++
@@ -1,6 +0,0 @@
-#Uses python3
-n = int(input())
-a = [int(x) for x in input().split()]
-
-arr = sorted(a)
-print(arr[n - 1] * arr[n - 2])
--- week1/Challenge1_2MaximumPairwiseProduct/stressTest.py
+++ week1/Challenge1_2MaximumPairwiseProduct/stressTest.py
@@ -1,21 +1,38 @@
 #git config --global core.autocrlf true
+import random
 
-result = 0
-
-arr = []
-n = len(arr)
+iterations = 1
 
 def mpp(arr):
     arrSorted = sorted(arr)
-    return (arrSorted[n - 1] * arrSorted[n - 2])
+    arrLen = len(arrSorted)
+    return (arrSorted[arrLen - 1] * arrSorted[arrLen - 2])
 
 
-def max_pairwise_product(arr, n)
+def max_pairwise_product(arr):
     result = 0
-
+    n = len(arr)
     for i in range(0, n):
         for j in range(i+1, n):
-            if a[i]*a[j] > result:
-                result = a[i]*a[j]
+            if arr[i]*arr[j] > result:
+                result = arr[i]*arr[j]
 
     return result
+
+
+while True:
+    n = random.randint(2, 1000)
+    arr=[]
+
+
+    for i in range(random.randint(2, n)):
+        arr.append(random.randint(0, 200000))
+
+    if mpp(arr) == max_pairwise_product(arr):
+        print("Good Job!")
+    else:
+        print("Wrong")
+        break
+
+    iterations+=1
+    print(iterations)
