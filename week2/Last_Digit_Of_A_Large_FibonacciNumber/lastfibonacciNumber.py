9648d19df89c2e8c4dfc759e7b1fbe68d9f90ea1
Jay Guzman <j.d.guzman.v@gmail.com>
2018-02-24 15:44:22 -0700

    Faster algorithm,but not faster enough

--- week2/Last_Digit_Of_A_Large_FibonacciNumber/lastfibonacciNumber.py
+++ week2/Last_Digit_Of_A_Large_FibonacciNumber/lastfibonacciNumber.py
@@ -1,34 +1,30 @@
 # Uses python3
-
 def calc_fib_mine(n):
     arr = [0, 1]
+    fibList = []
+    m =int((n+2)/2)
     counter = 0
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
 
+    if n == 0 or n == 1:
+        return n
     else:
+        for i in range(2, m + 2 ) :
 
-        for i in range(2, n + 1):
             arr[0] += arr[1]
+            counter+= 1
+            if( counter == n -1):
+                fibList.append(arr[0])
+                break
+            #print(arr[0])
+
             arr[1] += arr[0]
             counter+=1
-            if counter == n/2:
+            if(counter == n -1):
+                fibList.append(arr[1])
                 break
+            #print(arr[1])
 
-            print (arr[0])
-            print (arr[1])
-        if n % 2 == 0:
-            stringLen= int(len(str(arr[0])))
-            #return str(arr[0])[stringLen - 1]
-            return arr[0]
-
-        else:
-            stringLen= int(len(str(arr[1])))
-            #return str(arr[1])[stringLen - 1]
-            return arr[1]
-
+        return fibList[0]
 
 n = int(input())
 print(calc_fib_mine(n))
