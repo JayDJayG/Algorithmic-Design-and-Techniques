d6fff3506a44eea2ee1b2bb2ecdb9c99c8bcd7a3
Jay Guzman <j.d.guzman.v@gmail.com>
2018-02-23 21:14:00 -0700

    Small changes in the proposed Algorithm

--- week2/Last_Digit_Of_A_Large_FibonacciNumber/lastfibonacciNumber.py
+++ week2/Last_Digit_Of_A_Large_FibonacciNumber/lastfibonacciNumber.py
@@ -1,7 +1,7 @@
 # Uses python3
 
 def calc_fib_mine(n):
-    arr = [0, 0, 1]
+    arr = [0, 1]
     if n == 0:
         return 0
     elif n == 1:
@@ -10,13 +10,16 @@ def calc_fib_mine(n):
     else:
 
         for i in range(2, n + 1):
-            arr[0] = arr [1] + arr[2]
-            arr[1] = arr[2]
-            arr[2] = arr[0]
-
-        stringLen= int(len(str(arr[0])))
-        return str(arr[0])[stringLen - 1]
+            arr[0] += arr[1]
+            arr[1] += arr[0]
 
 
+        if n % 2 == 0:
+            stringLen= int(len(str(arr[0])))
+            return str(arr[0])[stringLen - 1]
+        else:
+            stringLen= int(len(str(arr[1])))
+            return str(arr[1])[stringLen - 1]
+                
 n = int(input())
 print(calc_fib_mine(n))
