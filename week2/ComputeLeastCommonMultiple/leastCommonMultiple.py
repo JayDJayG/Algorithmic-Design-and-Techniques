ce5948f9a88f4c4bd3e62d92e497b449622734a8
Jay Guzman <j.d.guzman.v@gmail.com>
2018-02-28 18:14:07 -0700

    Starting with test of the first prototype

--- week2/ComputeLeastCommonMultiple/leastCommonMultiple.py
+++ week2/ComputeLeastCommonMultiple/leastCommonMultiple.py
@@ -1,10 +1,30 @@
+# Uses python3
+import sys
+
+def gcd_euclidean(a, b):
+
+    if a > b or a == b:
+        big = a
+        small = b
+    else:
+        big = b
+        small = a
+
+    rest =0
+    total = 0
+
+
+    while True:
+        rest = big % small
+        big = small
+        small = rest
+
+        if rest == 0:
+            return big
 
 def lcm(a, b):
-    
 
-
-
-
+    return ((a*b)/gcd_euclidean(a, b))
 
 
 if __name__ == '__main__':
